508e6d12dd464610d247c1a0b77a642d85aae0e5
Kethmie Perera <47343068+katp1998@users.noreply.github.com>
2022-07-04 09:23:18 +0530

    Working on AddTask
    Signed-off-by: Kethmie Perera <47343068+katp1998@users.noreply.github.com>

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -28,13 +28,19 @@ function App() {
 
   //Delete a task
   const deleteTask = (id) =>{
-    console.log('delete', id);
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
+  //toggle reminder
+
+  const ToggleReminder = (id) =>{
+    setTasks(tasks.map((task) => task.id === id? {...task, reminder: ! task.reminder}: task))
   }
 
   return (
     <div className="container">
       <Header  />
-      <Tasks tasks = {tasks} onDelete={deleteTask} />
+      {tasks.length > 0? (<Tasks tasks = {tasks} onDelete={deleteTask} onToggle = {ToggleReminder}/>) : ('No Tasks to show') }
     </div>
   );
 }
---
+++ react-task-tracker/src/components/AddTask.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddTask = () => {
+  return (
+    <form>
+      <div>AddTask</div>
+    </form>
+  );
+};
+
+export default AddTask;
